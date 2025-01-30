da32e1eddd9181dbe358ebba73e56e26f725c914
0x19C <golddream.ws@gmail.com>
2025-01-30 22:28:51 +0000

    Generate dataset thumbnail on creating

--- backend/src/module/image.py
+++ backend/src/module/image.py
@@ -1,9 +1,78 @@
-from typing import IO
+import os
+import json
+
+from typing import IO, Union
 
 from PIL import Image
 
-async def get_image_metadata(fp: IO[bytes]) -> dict:
-    img = Image.open(fp)
-    metadata = img._getexif()
 
-    return metadata
+def get_thumbnail_of_image(file_path, target_height=128):
+    img = Image.open(file_path)
+    width, height = img.size
+    return img.resize((int(target_height * width / height), target_height), Image.Resampling.LANCZOS)
+
+
+def generate_thumbnail_of_dataset(directory, target_height=128):
+    if os.path.exists(os.path.join(directory, "thumbnail.png")):
+        return
+    if not os.listdir(directory):
+        return
+    images = [get_thumbnail_of_image(os.path.join(directory, filename), target_height) for filename in os.listdir(directory) if filename != "thumbnail.png"]
+
+    total_width = sum(img.width for img in images)
+    combined_image = Image.new("RGB", (total_width, target_height))
+    x_offset = 0
+    for img in images:
+        combined_image.paste(img, (x_offset, 0))
+        x_offset += img.width
+
+    combined_image.save(os.path.join(directory, "thumbnail.png"))
+
+
+def get_image_metadata(fp: Union[str, bytes, os.PathLike[str], os.PathLike[bytes],IO[bytes]]) -> dict:
+    img = Image.open(fp)
+
+    info_dict = {}
+    for key, value in img.info.items():
+        if isinstance(value, bytes):
+            try:
+                info_dict[key] = value.decode("utf-8", errors="ignore")  # Decode safely
+            except UnicodeDecodeError:
+                info_dict[key] = None  # Remove non-decodable binary data
+        else:
+            info_dict[key] = value
+
+    return {
+        "format": img.format,
+        "mode": img.mode,
+        "size": img.size,
+        "width": img.width,
+        "height": img.height,
+        "palette": img.palette,
+        "info": str(info_dict),
+    }
+
+
+def clone_image(fp: Union[str, bytes, os.PathLike[str], os.PathLike[bytes],IO[bytes]], brightness: float, noise: float):
+    img = Image.open(fp)
+    print("getdata", len(list(img.getdata())))
+
+    img.thumbnail([128, 128])
+    print("thumbnail getdata", len(list(img.getdata())))
+
+    print("getexif", img.getexif())
+    print("getextrema", img.getextrema())
+    print("getxmp", img.getxmp())
+    print("attributes", {
+        "filename": img.filename,
+        "format": img.format,
+        "mode": img.mode,
+        "size": img.size,
+        "width": img.width,
+        "height": img.height,
+        "palette": img.palette,
+        "info": img.info,
+        "is_animated": img.is_animated,
+        "n_frames": img.n_frames,
+        "has_transparency_data": img.has_transparency_data,
+    })
--- backend/src/routes/datasets.py
+++ backend/src/routes/datasets.py
@@ -20,11 +20,11 @@ from src.auth.jwthandler import (
     get_current_user,
 )
 
-from src.module.image import get_image_metadata
+from src.module.image import get_image_metadata, clone_image, generate_thumbnail_of_dataset
 
 router = APIRouter(tags=["Datasets"])
 
-UPLOAD_DIR="./storage/datasets"
+UPLOAD_DIR = "./storage/datasets"
 
 @router.post(
     "/datasets", response_model=DataSetOutSchema, dependencies=[Depends(get_current_user)],
@@ -46,12 +46,14 @@ async def create_dataset(
         with open(file_path, "wb") as buffer:
             readable_file = await file.read()
             buffer.write(readable_file)
-            metadata = await get_image_metadata(io.BytesIO(readable_file))
+            metadata = get_image_metadata(file_path)
         images.append({
             "name": file.filename,
             "file_path": file_path,
             "metadata": metadata
         })
+
+    generate_thumbnail_of_dataset(dataset_path)
 
     return await crud.create_dataset(DataSetInSchema.model_validate({
         "name": name or directory_name,
@@ -64,6 +66,62 @@ async def create_dataset(
         ImageInSchema.model_validate(ele)
         for ele in images
     ])
+
+
+@router.post(
+    "/datasets/{dataset_id}", response_model=DataSetOutSchema, dependencies=[Depends(get_current_user)],
+    summary="Clone dataset by changing brightness or by adding gaussian noise.",
+)
+async def clone_dataset(
+    dataset_id: int,
+    brightness: float = Form(...),
+    noise: float = Form(...),
+    current_user: UserOutSchema = Depends(get_current_user)
+) -> DataSetOutSchema:
+    try:
+        if current_user.is_admin:
+            dataset = await DataSetOutSchema.from_queryset_single(DataSets.get(id=dataset_id))
+        dataset = await DataSetOutSchema.from_queryset_single(DataSets.get(id=dataset_id, user_id=current_user.id))
+    except DoesNotExist as e:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Dataset does not exist!",
+        ) from e
+
+    directory_name = str(uuid.uuid4())
+    dataset_path = f"{UPLOAD_DIR}/{directory_name}"
+    # if not os.path.exists(dataset_path):
+    #     os.mkdir(dataset_path)
+
+    images = []
+    for idx, image in enumerate(dataset.dataset_images):
+        origin_file_path = image.file_path
+        clone_image(origin_file_path, brightness, noise)
+
+        file_path = f"{dataset_path}/{brightness}_{noise}_{str(idx)}-{image.name}"
+        # with open(origin_file_path, "rb") as file:
+        #     with open(file_path, "wb") as buffer:
+        #         readable_file = file.read()
+        #         buffer.write(readable_file)
+        #         metadata = await get_image_metadata(io.BytesIO(readable_file))
+        # images.append({
+        #     "name": file.filename,
+        #     "file_path": file_path,
+        #     "metadata": metadata
+        # })
+    return dataset
+
+    # return await crud.create_dataset(DataSetInSchema.model_validate({
+    #     "name": directory_name,
+    #     "directory_path": dataset_path,
+    #     "user_id": current_user.id,
+    #     "sample_count": len(images),
+    #     "metadata": [ele.get("metadata", None) for ele in images],
+    #     "parent_id": None,
+    # }), [
+    #     ImageInSchema.model_validate(ele)
+    #     for ele in images
+    # ])
 
 
 @router.get(
