753a2bc279c567546ee815c77c2038b69905269f
0x19C <golddream.ws@gmail.com>
2025-02-01 06:34:38 +0900

    Position Fixed for NavBar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,45 +1,58 @@
 <template>
-  <fwb-navbar>
-    <template #logo>
-      <fwb-navbar-logo alt="Flowbite logo" image-url="/login.png" link="/">
-      </fwb-navbar-logo>
-    </template>
-    <template #default="{ isShowMenu }">
-      <fwb-navbar-collapse v-if="isLoggedIn" :is-show-menu="isShowMenu">
-        <fwb-navbar-link class="py-2" is-active link="/">
-          Home
-        </fwb-navbar-link>
-        <fwb-navbar-link class="py-2" link="/dataset"> Dataset </fwb-navbar-link>
-        <fwb-navbar-link class="py-2" link="/model"> Model </fwb-navbar-link>
-        <fwb-navbar-link class="py-2" link="/training"> Training </fwb-navbar-link>
-        <fwb-dropdown :text="username" align-to-end>
-          <fwb-list-group class="w-20">
-            <fwb-list-group-item>
-              <fwb-navbar-link class="py-1" link="/register">
-                Create a new Account
-              </fwb-navbar-link>
-            </fwb-list-group-item>
-            <fwb-list-group-item>
-              <span class="py-1 cursor-pointer" @click="() => logout()">
-                Logout
-              </span>
-            </fwb-list-group-item>
-          </fwb-list-group>
-        </fwb-dropdown>
-      </fwb-navbar-collapse>
-      <fwb-navbar-collapse v-else :is-show-menu="isShowMenu">
-        <fwb-a class="py-2" href="/register"> Register </fwb-a>
-        <fwb-a class="py-2" href="/login"> Login </fwb-a>
-      </fwb-navbar-collapse>
-    </template>
-  </fwb-navbar>
-  <div v-if="isVisible" class="vp-raw grid gap-2 absolute right-10 w-4xs">
-    <fwb-alert class="border-t-4 rounded-none" :type="messageType">
-      <span>{{ messageTitle }}</span>
-      <ul v-if="messageList.length" class="mt-1.5 ml-4 list-disc list-inside">
-        <li v-for="message in messageList">{{ message }}</li>
-      </ul>
-    </fwb-alert>
+  <div class="fixed w-screen z-50 shadow top-0 left-0">
+    <div class="container mx-auto">
+      <fwb-navbar>
+        <template #logo>
+          <fwb-navbar-logo alt="Flowbite logo" image-url="/login.png" link="/">
+          </fwb-navbar-logo>
+        </template>
+        <template #default="{ isShowMenu }">
+          <fwb-navbar-collapse v-if="isLoggedIn" :is-show-menu="isShowMenu">
+            <fwb-navbar-link class="py-2" is-active link="/">
+              Home
+            </fwb-navbar-link>
+            <fwb-navbar-link class="py-2" link="/dataset">
+              Dataset
+            </fwb-navbar-link>
+            <fwb-navbar-link class="py-2" link="/model">
+              Model
+            </fwb-navbar-link>
+            <fwb-navbar-link class="py-2" link="/training">
+              Training
+            </fwb-navbar-link>
+            <fwb-dropdown :text="username" align-to-end>
+              <fwb-list-group class="w-20">
+                <fwb-list-group-item>
+                  <fwb-navbar-link class="py-1" link="/register">
+                    Create a new Account
+                  </fwb-navbar-link>
+                </fwb-list-group-item>
+                <fwb-list-group-item>
+                  <span class="py-1 cursor-pointer" @click="() => logout()">
+                    Logout
+                  </span>
+                </fwb-list-group-item>
+              </fwb-list-group>
+            </fwb-dropdown>
+          </fwb-navbar-collapse>
+          <fwb-navbar-collapse v-else :is-show-menu="isShowMenu">
+            <fwb-a class="py-2" href="/register"> Register </fwb-a>
+            <fwb-a class="py-2" href="/login"> Login </fwb-a>
+          </fwb-navbar-collapse>
+        </template>
+      </fwb-navbar>
+      <div v-if="isVisible" class="vp-raw grid gap-2 absolute right-10 w-4xs">
+        <fwb-alert class="border-t-4 rounded-none" :type="messageType">
+          <span>{{ messageTitle }}</span>
+          <ul
+            v-if="messageList.length"
+            class="mt-1.5 ml-4 list-disc list-inside"
+          >
+            <li v-for="message in messageList">{{ message }}</li>
+          </ul>
+        </fwb-alert>
+      </div>
+    </div>
   </div>
 </template>
 
